0dcf91e5da2c24f2113566d07db06aba0ee04cff
lizus <lizus.com@gmail.com>
2021-05-18 18:57:18 +0800

    20210518

--- optimize/action/wp_dashboard_setup.php
+++ optimize/action/wp_dashboard_setup.php
@@ -10,5 +10,6 @@ add_action( 'wp_dashboard_setup', function (){
   remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );  // Recent Drafts
   remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );   // WordPress blog
   remove_meta_box( 'dashboard_secondary', 'dashboard', 'side' );   // Other WordPress News
+  remove_meta_box( 'dashboard_php_nag', 'dashboard', 'normal' );//隐藏php更新提醒
   // use 'dashboard-network' as the second parameter to remove widgets from a network dashboard.
 });
